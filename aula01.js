20297adb68b565518f32484c63b2f4994804b9af
Matheus <matheusyagami147@gmail.com>
2021-09-21 12:51:55 -0300

    criado o arquivo de aula01: trabalhando com variáveis

---
+++ aula01.js
@@ -0,0 +1,15 @@
+console.log('meu primeiro programa: trabalhando com variáveis')
+
+const idade = 23
+
+console.log('idade')
+console.log(idade)
+console.log(idade + 2)
+console.log(idade - 2)
+console.log(idade * 2)
+console.log(idade / 2)
+
+const idadeSomada = idade + 2
+console.log(idadeSomada)
+
+
