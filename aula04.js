06b31fe7fc861e7c22c12d8d57f0440a5cf63bb2
Matheus <matheusyagami147@gmail.com>
2021-09-21 14:38:39 -0300

    criado o arquivo de aula04.js sobre conversão de tipos

---
+++ aula04.js
@@ -0,0 +1,7 @@
+console.log('conversão de tipos')
+
+console.log('ano' + 2021) //ano2021
+console.log('2' + '2') //22
+console.log(parseInt('2') + parseInt('2')) //4
+console.log('10' / '2') //5 automaticamente ele converte para inteiro
+console.log('Matheus' / '2') //NaN, ou seja, tentou converter o Matheus para número, porém não conseguiu
