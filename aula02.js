9350747580174afbf1816bed2d3886058b3bf609
Matheus <matheusyagami147@gmail.com>
2021-09-21 13:24:21 -0300

    criado o arquivo de aula02.js referente às boas práticas com variáveis

---
+++ aula02.js
@@ -0,0 +1,17 @@
+console.log('trabalhando com variáveis')
+//js é case sensitive
+
+const Idade = 34
+const idade = 23
+const nome = "Matheus"
+const ano = 2021
+
+//boas práticas com variáveis
+
+console.log("Idade em maiúscula:", Idade)
+console.log("idade em minuscula:", idade)
+console.log(nome)
+console.log(ano)
+
+//não fazer
+//mes = 'Janeiro'
